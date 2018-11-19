934b26e54343e59307188503dd0086d04b0b9ca6
nathalia <45159220+nathalia6@users.noreply.github.com>
2018-11-19 00:35:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+  <header class="container">
+        <?php include("include.php");?>
+    </header>
+    
+    <title>Inicio</title>
+
+</head>
+
+</div>
+
+    
+
+     <style type="text/css">
+          .btn-circle.btn-xl {
+    width: 70px;
+    height: 70px;
+    padding: 10px 16px;
+    border-radius: 35px;
+    font-size: 24px;
+    line-height: 1.33;
+}
+
+.btn-circle {
+    width: 30px;
+    height: 30px;
+    padding: 6px 0px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 12px;
+    line-height: 1.42857;
+    }
+     .btn-circle.btn-lg {
+  width: 50px;
+  height: 50px;
+  padding: 10px 16px;
+  font-size: 18px;
+  line-height: 1.33;
+  border-radius: 25px;
+}
+
+
+      </style>
+
+<div class="panel-body" align="center">
+   <h1 class="text-info" ><strong>LOCALIZAR CÓDIGO</strong></h1>
+                          
+        <button class="btn btn-info btn-circle btn-xl">
+        <a style="text-decoration:none" href="cpus/visualizar_cpu"><span class="icon-windows"></span>
+        </a>
+        </button>
+        
+        <button class="btn btn-info btn-circle btn-xl">
+        <a style="text-decoration:none" href="laptops/visualizar_laptops"><span class="icon-laptop"></span></a>
+        </button>
+         
+        <button  class="btn btn-info btn-circle btn-xl">
+        <a style="text-decoration:none" href="monitores/visualizar_monitores"><span class="icon-display"></span> 
+        </a>
+        </button>
+        
+        <button  class="btn btn-info btn-circle btn-xl">
+        <a style="text-decoration:none" href="impresoras/visualizar_impresoras"><span class="icon-printer"></span>
+        </a>
+        </button>
+</div>
+
+</body>
+</html>
